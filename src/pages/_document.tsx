3af631e4f3fc4674950a98bf8e3837fb91a9bd16
dhantosy <dhantosy@gmail.com>
2023-11-02 15:49:04 +0800

    feat: added analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head />
       <body>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-5Z945BXL2P" strategy='afterInteractive' />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-5Z945BXL2P');
+        `}
+        </Script>
         <Main />
         <NextScript />
       </body>
